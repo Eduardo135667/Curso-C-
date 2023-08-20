d87cf2be2ce542236b009bf211a6db161c66faa4
Eduardo135667 <el2210@gmail.com>
2023-08-20 13:11:37 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite seu nome: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Olá {name}!");
+Console.Write("Digite o ano do seu nascimento: ");
+int year =  int.Parse(Console.ReadLine());
+int age = 2022 - year;
+Console.WriteLine($"Vovê tem {age} anos,");
+
